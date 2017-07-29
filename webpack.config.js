4e157a345dc2636822218d4fae3b3cca3cc8f6b3
Jeremie <forge@localhost.localdomain>
2017-07-29 18:14:03 +0200

    Folder reorganization
    Reorganized folders to match with GitHub Pages static server
    Project now available from the /app route

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, 'app');
 var APP_DIR = path.resolve(__dirname, 'src');
 var ROOT_DIR = path.resolve(__dirname, '');
 
