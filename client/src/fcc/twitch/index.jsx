7896bb8e1787cfcfd02b1265d12da2e37b7b19bd
jvdsande <jeremie.vandersande@gmail.com>
2017-08-27 01:13:25 +0200

    Finished commenting the Twitch project

--- client/src/fcc/twitch/index.jsx
+++ client/src/fcc/twitch/index.jsx
@@ -1,14 +1,31 @@
+/******************************************************************************/
+/* file: index.jsx                                                            */
+/* author: Jeremie van der Sande                                              */
+/******************************************************************************/
+/* Entry point and main component for the Pomodoro project                    */
+/******************************************************************************/
+
+
+/* Import React as our main framework                                         */
 import React, {Component} from 'react'
+
+/* Import React DOM to inject our components                                  */
 import ReactDOM from 'react-dom'
 
+/* Import jsonp to handle the JSON requests                                   */
 import jsonp from 'jsonp'
 
+/* Import the custom styles from 'styles.jsx'.
+ * 'styled-components' is used to keep the best of both the React and CSS
+ * worlds                                                                     */
 import {
   Board, Channel,
   ChannelSpacer,
   Name, Status, Title, Now,
 } from './styles'
 
+
+/* Create an array of channel to watch                                        */
 const channelNames = [
   'freecodecamp',
   'salakar',
@@ -19,88 +36,132 @@ const channelNames = [
   'lobosjr',
 ]
 
+
+/* The Twitch component shows a list of Twitch channels, with the corresponding
+ * status. It's the root of our appliation and renders the whole page         */
 class Twitch extends Component {
   constructor() {
     super()
 
-    this.channels = {}
+    /* Prepare an named array to hold the status of each channel              */
     this.state = {
-      loaded: 0
+      channels: {}
     }
 
+    /* Load information about the channels                                    */
+    this.refresh()
 
-    channelNames.map(c => {
-      jsonp(`https://wind-bow.gomix.me/twitch-api/streams/${c}`, (err, data) => {
-        let loaded = this.state.loaded
-        if(this.channels[c] !== undefined) {
-          loaded ++
-        } else {
-          this.channels[c] = {}
-        }
+    /* Refresh the status of the channels every minute                        */
+    setInterval(this.refresh, 60 * 1000)
+  }
 
-        this.channels[c].streams = data
+  /* Use the wind-bow.gomix API to fetch data from Twitch                     */
+  loadFromTwitch = (channel, path) => {
+    /* Load the required path on the passed channel                           */
+    jsonp(`https://wind-bow.gomix.me/twitch-api/${path}/${channel}`, (err, data) => {
+      /* Get the current list of channels                                     */
+      let channels = this.state.channels
 
-        this.setState({loaded})
-      })
-      jsonp(`https://wind-bow.gomix.me/twitch-api/channels/${c}`, (err, data) => {
-        let loaded = this.state.loaded
-        if(this.channels[c] !== undefined) {
-          loaded ++
-        } else {
-          this.channels[c] = {}
-        }
+      /* Initialize the channel's object if it does not exist                 */
+      channels[channel] = channels[channel] || {}
 
-        this.channels[c].info = data
+      /* Convert the path to an Object key to be used in render               */
+      const pathKeys = {
+        channels: 'info',
+        streams: 'streams'
+      }
+      let key = pathKeys[path]
 
-        this.setState({loaded})
-      })
+      /* Add the loaded data to the channel's object                          */
+      channels[channel][key] = data
+
+      /* Update the list of channels                                          */
+      this.setState({channels})
     })
+  }
 
+  /* Refresh the status of the channels                                       */
+  refresh = () => {
+    /* We start by looping through the channelNames array                     */
+    channelNames.map(c => {
+      /* First we load the general channel infos                              */
+      this.loadFromTwitch(c, 'channels')
+
+      /* Then we load infos specific to the current stream                    */
+      this.loadFromTwitch(c, 'streams')
+    })
   }
 
   render() {
-    let channels = []
-    if(this.state.loaded == channelNames.length) {
-      channels = Object.keys(this.channels)
+    /* Get all the loaded channels name                                       */
+    let channels = Object.keys(this.state.channels)
 
+    /* Filter out the channels where either the streams infos or general infos
+     * are not yet loaded                                                     */
+    channels = channels.filter(c => {
+      c = this.state.channels[c]
+      return (c.info && c.streams)
+    })
 
-      channels = channels.sort((a, b) => {
-        a = this.channels[a]
-        b = this.channels[b]
-        if(a.streams.stream) {
-          if(b.streams.stream) {
-            return a.info.display_name[0].toUpperCase() > b.info.display_name[0].toUpperCase() ? 1 : -1
-          }
-          return -1
-        } else {
-          if(b.streams.stream) {
-            return 1
-          }
+    /* Sort the channels by name, with the online channels showing first      */
+    channels = channels.sort((a, b) => {
+      a = this.state.channels[a]
+      b = this.state.channels[b]
+
+      /* Check if A is online                                                 */
+      if(a.streams.stream) {
+        /* If it is, check if B is online                                     */
+        if(b.streams.stream) {
+          /* If B and A are both online, sort by name                         */
           return a.info.display_name[0].toUpperCase() > b.info.display_name[0].toUpperCase() ? 1 : -1
         }
-      })
-    }
+
+        /* If only A is online, then A comes first                            */
+        return -1
+      } else {
+        /* If A is not online, check if B is online                           */
+        if(b.streams.stream) {
+          /* If B is online and not A, then B comes first                     */
+          return 1
+        }
+
+        /* If both are offline, sort by name                                  */
+        return a.info.display_name[0].toUpperCase() > b.info.display_name[0].toUpperCase() ? 1 : -1
+      }
+    })
+
+    /* Render our component                                                   */
     return (
       <Board>
+        {/* First we add a title to our list of channels                      */}
         <Title>Twitch Channels</Title>
-        {channels.map(c => {
-          let channel = this.channels[c]
 
+        {/* Then we loop through our sorted list                              */}
+        {channels.map(c => {
+          /* Get the current channel                                          */
+          let channel = this.state.channels[c]
+
+          /* Check if the channel is online                                   */
           let online = channel.info.status && channel.streams.stream
+          /* Get the name of the stream, if any                               */
           let status = (channel.info.status || '')
 
-          if(channel.info) {
-            return (
-              <Channel online={online} src={channel.info.logo || './default.png'} href={channel.info.url || ''}>
-                <Name>{channel.info.display_name || c}</Name>
-                {online ? <Status><Now>Now Streaming:</Now>{status}</Status> : null}
+          return (
+            <Channel online={online} src={channel.info.logo || './default.png'} href={channel.info.url || ''}>
+              {/* Create a Channel element reflecting the status and showing the
+                * logo of the channel                                         */}
 
-              </Channel>
-            )
-          }
+              {/* Add the channel's name                                      */}
+              <Name>{channel.info.display_name || c}</Name>
 
-          return null
+              {/* If the channel is online, append information on the stream  */}
+              {online ? <Status><Now>Now Streaming:</Now>{status}</Status> : null}
+            </Channel>
+          )
         })}
+
+        {/* The ChannelSpacer allows to align channels to the right despite the
+          * flexbox 'space-around' property                                   */}
         <ChannelSpacer />
         <ChannelSpacer />
         <ChannelSpacer />
@@ -112,6 +173,9 @@ class Twitch extends Component {
   }
 }
 
+
+/* The <div id='root'> element of our HTML will render our components         */
 let target = document.getElementById('root')
-ReactDOM.render(
-  <Twitch />, target)
+
+/* Render the Twitch project                                                  */
+ReactDOM.render(<Twitch />, target)
