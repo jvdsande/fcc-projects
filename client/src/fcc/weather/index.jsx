a1002c641d9b9a8f7b212f46c9d55c8087cbf63d
jvdsande <jeremie.vandersande@gmail.com>
2017-08-27 13:03:09 +0200

    Moved the 'public' folder to a 'docs' folder to use GitHub Pages 'repo/docs' entry point

--- client/src/fcc/weather/index.jsx
+++ client/src/fcc/weather/index.jsx
@@ -1,66 +1,91 @@
+/******************************************************************************/
+/* file: index.jsx                                                            */
+/* author: Jeremie van der Sande                                              */
+/******************************************************************************/
+/* Entry point and main component for the Weather project                     */
+/******************************************************************************/
+
+
+/* Import React as our main framework                                         */
 import React, {Component} from 'react'
+
+/* Import React DOM to inject our components                                  */
 import ReactDOM from 'react-dom'
 
-import get from 'simple-get'
+/* Import jsonp to use the weather AP                                         */
+import jsonp from 'jsonp'
 
-import {Sun, Particles} from './particles'
+/* Import the custom styles from 'styles.jsx'.
+ * 'styled-components' is used to keep the best of both the React and CSS
+ * worlds                                                                     */
 import {
   Background, Foreground,
   Panel, ErrorPanel,
   Temp, Unit, Type, City,
 } from './styles'
 
+/* Import the Sun and Particles components                                    */
+import {Sun, Particles} from './particles'
+
+
+/* The Weather component is the main component of our app. It renders a weather
+ * card inspired by Google's Material Design                                  */
 class Weather extends Component {
   constructor() {
     super()
 
+    /* Prepare our React state to store options and weather information       */
     this.state = {
       weather: {},
       unit: 'C',
       error: '',
+      position: {lat: 0, lon: 0}
     }
 
-    this.position = {
-      lat: 0,
-      lon: 0,
-    }
-
-
+    /* Get the current position                                               */
     this.getPosition()
 
-    // Refresh weather every minute
+    /* Refresh weather every minute                                           */
     setInterval(this.refresh, 60 * 1000)
 
-    // Refresh position every 30 minutes
+    /* Refresh position every 30 minutes                                      */
     setInterval(this.getPosition, 30 * 60 * 1000)
   }
 
+  /* Use the built-in navigator API to get the user's position                */
   getPosition = () => {
+    /* Check if the navigator supports geolocation                            */
     if(navigator.geolocation) {
+
+      /* Get the user's position                                              */
       navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude
         let lon = position.coords.longitude
 
-        this.position = {lat, lon}
+        /* Update the state with the new position                             */
+        this.setState({position: {lat, lon}})
 
-        this.refresh()
-      }, (err) => {
+        /* Queue a refresh of the app after the last state update             */
+        setTimeout(this.refresh, 0)
+      },
+      /* In case of error, show an error message                              */
+      (err) => {
         let error = 'Error: please use HTTPS'
         this.setState({error})
       })
-    } else {
+    }
+    /* If not, display an error message accordingly                           */
+    else {
       let error = 'Error: your browser does not support location-based services'
       this.setState({error})
     }
   }
 
+  /* Check the weather through the FCC weather API using the current position */
   refresh = () => {
-    let lat = this.position.lat
-    let lon = this.position.lon
-    get.concat(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`, (err, res, data) => {
-      let weather = (JSON.parse(data))
-      this.setState({weather})
-    })
+    let lat = this.state.position.lat
+    let lon = this.state.position.lon
+    jsonp(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`, (err, weather) => this.setState({weather}))
   }
 
   changeUnit = () => {
@@ -126,6 +151,7 @@ class Weather extends Component {
   }
 }
 
+/* If the page was loaded over http, redirect to the https version            */
 if(window.location.protocol == 'http:')
 {
   window.location.href = 'https://'+window.location.hostname+window.location.pathname
